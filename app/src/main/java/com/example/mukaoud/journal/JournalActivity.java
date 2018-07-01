50f6c603464112bf8049b7619959e103ee5d4969
Mostafa <mukaoud@yahoo.com>
2018-07-01 23:52:16 +0200

    google Authentication

--- app/src/main/java/com/example/mukaoud/journal/JournalActivity.java
+++ app/src/main/java/com/example/mukaoud/journal/JournalActivity.java
@@ -1,13 +1,103 @@
 package com.example.mukaoud.journal;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.net.PasswordAuthentication;
 
 public class JournalActivity extends AppCompatActivity {
+    private static final String TAG =JournalActivity.class.getName();
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user == null)
+        {
+            startActivity(new Intent(this,SignInActivity.class));
+            finish();
+        }
         setContentView(R.layout.activity_journal);
+        TextView userName = (TextView) findViewById(R.id.user_name);
+        if(user != null)
+            userName.setText(user.getDisplayName());
+/*
+        mAuth = FirebaseAuth.getInstance();
+        ContactsContract.CommonDataKinds.Email emailO = null;
+        PasswordAuthentication passwordO = null;
+
+        createAccount(emailO,passwordO);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            String uid = user.getUid();
+        }
+*/
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+    void createAccount(ContactsContract.CommonDataKinds.Email email, PasswordAuthentication password){
+
+        Log.e("Email and password ","email.toString()"+email.toString()+
+                password.toString()+" password.getPassword() = "+password.getPassword());
+
+        mAuth.createUserWithEmailAndPassword(email.toString(), password.toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(JournalActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void signOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(JournalActivity.this,SignInActivity.class));
+        finish();
     }
 }
